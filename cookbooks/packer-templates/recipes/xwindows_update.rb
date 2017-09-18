343b494106d8f928f5f9bd978977c0900bc19b13
Aline Freitas <aline@alinefreitas.com.br>
2017-09-18 18:02:05 -0300

    Use DSC for windows update setup.

---
+++ cookbooks/packer-templates/recipes/xwindows_update.rb
@@ -0,0 +1,25 @@
+mod = 'xWindowsUpdate'
+
+powershell_script "install-#{mod}" do
+  code <<-EOH
+  Install-PackageProvider -Name "Nuget" -Force
+  Install-Module #{mod} -Force
+  $env:PSModulePath = [System.Environment]::GetEnvironmentVariable("PSModulePath","Machine")
+  EOH
+  guard_interpreter :powershell_script
+  not_if "Get-Module -ListAvailable -Name #{mod}"
+end
+
+dsc_script 'activate-windows' do
+  imports 'xWindowsUpdate'
+  code <<-EOH
+  xWindowsUpdateAgent MuSecurityImportant
+  {
+    IsSingleInstance = 'Yes'
+    UpdateNow        = $true
+    Category         = @('Security','Important')
+    Source           = 'MicrosoftUpdate'
+    Notifications    = 'Disabled'
+  }
+  EOH
+end
